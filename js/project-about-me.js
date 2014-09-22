b147a438d7a9729a43c0c65b91835ef2c3a990fd
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2014-09-22 14:16:11 -0600

    Added lost of requirements: Details, Blockquote, Text-shadow, address, fontface, Google Font; Removed unused fonts

--- js/project-about-me.js
+++ js/project-about-me.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
   $('.popup-with-move-anim').magnificPopup({
 		type: 'inline',
-
 		fixedContentPos: false,
 		fixedBgPos: true,
-
 		overflowY: 'auto',
-
 		closeBtnInside: true,
 		preloader: false,
-
 		midClick: true,
 		removalDelay: 300,
 		mainClass: 'my-mfp-slide-bottom'
 	});
+
+  smoothScroll.init({
+  	easing: 'easeInOutCubic',
+  	updateURL: false,
+  	speed: 800
+  });
+
 });
+var epochToBirth = 772702259;
+var getAge = function(){
+	var nowSec = Math.floor(Date.now()/1000);
+	$('#age').text(nowSec - epochToBirth);
+}
+
+window.setInterval(getAge,1000);
