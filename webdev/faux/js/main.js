53c5d565d129d48167ce13dd3e2d122a526241a5
elliot627 <elliotwhitehead1@yahoo.com>
2014-12-11 10:12:07 -0700

    Added / adjusted content in 'About' panel; Changed color scheme; other various fixes;

--- webdev/faux/js/main.js
+++ webdev/faux/js/main.js
@@ -4,8 +4,20 @@ $(document).ready(function(){
 	$('.navbar-nav li a').click(function(e){
 		e.preventDefault();
 	});
-	
+
 	$('#title').fitText(1);
 	$('#subtitle').fitText(3);
 
+
+	smoothScroll.init({
+	  	easing: 'easeInOutCubic',
+	  	updateURL: false,
+	  	speed: 800
+  	});
+
+  	wow = new WOW({
+		offset: 100
+	});
+
+	wow.init();
 });
